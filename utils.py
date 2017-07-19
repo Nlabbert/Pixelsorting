c01f41f1e0888aabd5405ede7e672a09f4cc85b7
Nslabbert <delta991r@gmail.com>
2017-07-19 15:55:29 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,63 @@
+"""Utilities used in the Kadenze Academy Course on Deep Learning w/ Tensorflow.
+
+Creative Applications of Deep Learning w/ Tensorflow.
+Kadenze, Inc.
+Parag K. Mital
+
+Copyright Parag K. Mital, June 2016.
+"""
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import urllib
+import numpy as np
+import zipfile
+import os
+from scipy.misc import imsave
+
+def montage(images, saveto='montage.png'):
+    """Draw all images as a montage separated by 1 pixel borders.
+
+    Also saves the file to the destination specified by `saveto`.
+
+    Parameters
+    ----------
+    images : numpy.ndarray
+        Input array to create montage of.  Array should be:
+        batch x height x width x channels.
+    saveto : str
+        Location to save the resulting montage image.
+
+    Returns
+    -------
+    m : numpy.ndarray
+        Montage image.
+    """
+    if isinstance(images, list):
+        images = np.array(images)
+    img_h = images.shape[1]
+    img_w = images.shape[2]
+    n_plots = int(np.ceil(np.sqrt(images.shape[0])))
+    if len(images.shape) == 4 and images.shape[3] == 3:
+        m = np.ones(
+            (images.shape[1] * n_plots + n_plots + 1,
+             images.shape[2] * n_plots + n_plots + 1, 3)) * 0.5
+    elif len(images.shape) == 4 and images.shape[3] == 1:
+        m = np.ones(
+            (images.shape[1] * n_plots + n_plots + 1,
+             images.shape[2] * n_plots + n_plots + 1, 1)) * 0.5
+    elif len(images.shape) == 3:
+        m = np.ones(
+            (images.shape[1] * n_plots + n_plots + 1,
+             images.shape[2] * n_plots + n_plots + 1)) * 0.5
+    else:
+        raise ValueError('Could not parse image shape of {}'.format(
+            images.shape))
+    for i in range(n_plots):
+        for j in range(n_plots):
+            this_filter = i * n_plots + j
+            if this_filter < images.shape[0]:
+                this_img = images[this_filter]
+                m[1 + i + i * img_h:1 + i + (i + 1) * img_h,
+                  1 + j + j * img_w:1 + j + (j + 1) * img_w] = this_img
+    return m
